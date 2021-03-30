665225650263587058bb3cc3dba7b6773fb096f1
JANIKA\Disas <janika.suhonen2@gmail.com>
2021-03-30 12:45:15 +0300

    Linked project

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // take the current position = new position (0,0,0)
+        transform.position = new Vector3(0, 0, 0);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        // Gets the horizontal axis and saves it in horizontalInput variable
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+        transform.Translate(direction * _speed * Time.deltaTime);
+
+        //if player position on the y is greater than 0
+        //y position = 0
+        if(transform.position.y >= 0)
+        {
+            transform.position = new Vector3(transform.position.x, 0, transform.position.z);
+        }
+        else if (transform.position.y <= -3.8)
+        {
+            transform.position = new Vector3(transform.position.x, -3.8f, transform.position.z);
+        }
+
+        //if player on the x is greater than 11 
+        //then x pos = -11
+        //else if player on the x is less than -11
+        //x pos = 11
+    }
+}
